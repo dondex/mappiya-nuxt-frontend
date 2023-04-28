0cc4054a6db28c42004ac638be33bb3e7221c35d
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-04-28 21:33:16 +0800

    Add login setup

---
+++ pages/login.vue
@@ -0,0 +1,56 @@
+
+<template>
+    <div class="container d-flex justify-content-center">
+        <div class="d-flex">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3">
+                            <label for="">E-mail</label>
+                            <input type="text" class="form-control" v-model="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Password</label>
+                            <input type="password" class="form-control" v-model="password">
+                        </div>
+                        <div class="mt-4">
+                            <button class="btn btn-primary w-100" @click="login">Sign In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: 'roosevelt.kilback@example.com',
+            password: 'password',
+            user: null,
+            pending: null
+        }
+    },
+    methods: {
+        login() {
+            useFetch('http://app.mappiya.com/api/sanctum/token',
+                {
+                    method: 'post',
+                    body: {
+                        device_name: navigator.userAgent,
+                        email: this.email,
+                        password: this.password
+                    },
+                    headers: {
+                        accept: 'application/json',
+                    }
+                }
+            ).then((data) => {
+                console.log(data.data.value);
+            });
+        }
+    },
+};
+</script>
