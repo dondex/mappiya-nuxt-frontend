65ad71113064556078773316332f3fe08f1ed561
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-04-28 18:07:37 +0800

    add bootstrap 5 theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,30 @@
+<script setup>
+useHead({
+    link: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+            rel: 'stylesheet',
+            integrity: 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC',
+            crossorigin: 'anonymous'
+        },
+        {
+            href: 'css/main.css',
+            rel: 'stylesheet',
+        }
+    ],
+    script: [
+        {
+            src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+            integrity: 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
+            crossorigin: 'anonymous'
+        },
+        {
+            src: 'js/main.js'
+        }
+    ]
+});
+</script>
+
 <template>
     <div>
         <header>
@@ -14,16 +41,12 @@
         <div class="container mx-auto">
             <slot />
         </div>
-        
+
     </div>
 </template>
 
-<script setup>
-
-</script>
-
 <style scoped>
-    .router-link-exact-active {
-        color: #12b488;
-    }
+.router-link-exact-active {
+    color: #12b488;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,8 @@
 <template>
-    <div>  
-        <h2>Home</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt illo alias reiciendis dolore architecto, quis magnam soluta ad eaque amet aperiam libero quas et blanditiis praesentium sit tempora ut sed.</p>
+    <div> 
+        <button class="btn btn-primary">Button</button>
     </div>
 </template>
-
-<script setup>
-
-
-</script>
 
 <style scoped>
 
