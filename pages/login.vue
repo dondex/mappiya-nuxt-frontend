5168ea074683d85c4adf4c7350f2872521ef057e
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-05-30 19:00:17 +0800

    add responsiveness on login design

--- pages/login.vue
+++ pages/login.vue
@@ -9,7 +9,7 @@ definePageMeta({
     <div class="container d-flex align-items-center justify-content-center">
         <div class="d-flex justify-content-center w-100 mt-5">
             <div class="row w-100">
-                <div class="col-md-6 scooter-man-logo border-end border-2 border-dark">
+                <div class="d-none d-md-block col-md-6 scooter-man-logo border-end border-2 border-dark">
                     <img src="/img/scooter-man.png" />
                 </div>
                 <div class="col-md-5 d-flex flex-column p-5">
@@ -23,7 +23,7 @@ definePageMeta({
                         <input type="password" class="form-control py-3 shadow-sm" v-model="password"
                             placeholder="Password">
                     </div>
-                    <div class="mb-3 mt-2 d-flex justify-content-between">
+                    <div class="mb-3 mt-2 d-flex flex-wrap flex-md-row justify-content-center justify-content-md-between">
                         <div>
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -34,12 +34,12 @@ definePageMeta({
                             <a href="" class="text-dark">Forgot Password?</a>
                         </div>
                     </div>
-                    <div class="mb-3 mt-2 d-flex justify-content-between">
-                        <div class="text-center w-100 px-2">
-                            <button class="btn btn-info shadow-sm w-100">Register</button>
-                        </div>
+                    <div class="mb-3 mt-2 d-flex flex-column flex-md-row justify-content-center justify-content-md-between">
                         <div class="text-center w-100 px-2">
                             <button class="btn btn-info shadow-sm w-100" @click="login">Log In</button>
+                        </div>
+                        <div class="text-center w-100 px-2 mt-2 mt-md-0">
+                            <button class="btn btn-info shadow-sm w-100">Register</button>
                         </div>
                     </div>
                 </div>
