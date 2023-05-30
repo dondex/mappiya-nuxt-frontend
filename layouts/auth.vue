05afc6fe6c797b943f58dc24acbd0f890647d1dd
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-05-30 18:51:22 +0800

    Add login page design

---
+++ layouts/auth.vue
@@ -0,0 +1,67 @@
+<script setup>
+useHead({
+    link: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+            rel: 'stylesheet',
+            integrity: 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC',
+            crossorigin: 'anonymous'
+        },
+        {
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+            rel: 'stylesheet',
+            integrity: 'sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==',
+            crossorigin: 'anonymous'
+        },
+        {
+            href: 'css/main.css',
+            rel: 'stylesheet',
+        }
+    ],
+    script: [
+        {
+            src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+            integrity: 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
+            crossorigin: 'anonymous'
+        },
+        {
+            src: 'js/main.js'
+        }
+    ]
+});
+function logout(event) {
+    const token = useCookie('bearer_token')
+    token.value = null;
+    navigateTo('/login')
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-light bg-light shadow">
+        <div class="container">
+            <nuxt-link to="/" class="navbar-brand">
+                <img src="/img/mappiya-logo.png" alt="" width='55%'/>
+            </nuxt-link>
+        </div>
+    </nav>
+    <div>
+        <!-- output page content-->
+        <div class="container mx-auto">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+
+.btn-link {
+    font-size: smaller;
+    font-weight: 400;
+    /* text-decoration: underline; */
+    color: #fdfeff;
+    text-decoration: none;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,27 +1,45 @@
 <script setup>
 definePageMeta({
+    layout: "auth",
     middleware: 'auth'
 });
 </script>
 
 <template>
-    <div class="container vh-100 d-flex align-items-center justify-content-center">
-        <div class="d-flex justify-content-center">
-            <div class="d-flex">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex flex-column">
-                            <div class="mb-3">
-                                <label for="">E-mail</label>
-                                <input type="text" class="form-control" v-model="email">
+    <div class="container d-flex align-items-center justify-content-center">
+        <div class="d-flex justify-content-center w-100 mt-5">
+            <div class="row w-100">
+                <div class="col-md-6 scooter-man-logo border-end border-2 border-dark">
+                    <img src="/img/scooter-man.png" />
+                </div>
+                <div class="col-md-5 d-flex flex-column p-5">
+                    <div class="mb-3">
+                        <h2 class="fw-bold">Login In to your Account</h2>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control py-3 shadow-sm" v-model="email" placeholder="E-mail">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control py-3 shadow-sm" v-model="password"
+                            placeholder="Password">
+                    </div>
+                    <div class="mb-3 mt-2 d-flex justify-content-between">
+                        <div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Remember me</label>
                             </div>
-                            <div class="mb-3">
-                                <label for="">Password</label>
-                                <input type="password" class="form-control" v-model="password">
-                            </div>
-                            <div class="mt-4">
-                                <button class="btn btn-primary w-100" @click="login">Sign In</button>
-                            </div>
+                        </div>
+                        <div>
+                            <a href="" class="text-dark">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div class="mb-3 mt-2 d-flex justify-content-between">
+                        <div class="text-center w-100 px-2">
+                            <button class="btn btn-info shadow-sm w-100">Register</button>
+                        </div>
+                        <div class="text-center w-100 px-2">
+                            <button class="btn btn-info shadow-sm w-100" @click="login">Log In</button>
                         </div>
                     </div>
                 </div>
@@ -34,8 +52,8 @@ definePageMeta({
 export default {
     data() {
         return {
-            email: 'roosevelt.kilback@example.com',
-            password: 'password',
+            email: null,
+            password: null,
             user: null,
             pending: null,
             config: useRuntimeConfig()
